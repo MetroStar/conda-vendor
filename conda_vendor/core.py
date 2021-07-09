4d7c1212564ae22e546272602f342e642416febf
Robert Roundy <rob@ry.org>
2021-07-09 05:12:56 +0000

    basic flow in place

--- conda_vendor/core.py
+++ conda_vendor/core.py
@@ -1,18 +1,24 @@
+import hashlib
 import json
-import subprocess
+import logging as logger
+import os
 import pathlib
 import requests
-from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
-import yaml
-import hashlib
+import subprocess
 import sys
+import yaml
 from conda_lock.conda_lock import solve_specs_for_arch
 from conda_lock.src_parser.environment_yaml import parse_environment_file
-import logging as logger
-import os 
-class CondaLockWrapper:
-    pass
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
+def improved_download(url):
+    session = requests.Session()
+    retry = Retry(connect=5, backoff_factor=0.5)
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    return session.get(url)
 
 class CondaChannel:
     def __init__(
@@ -30,57 +36,35 @@ class CondaChannel:
                 }
         with open(environment_yml) as f:
             self.env_deps['environment'] = yaml.load(f, Loader=yaml.SafeLoader)
-        
+
+        self.platforms = ['linux-64', 'noarch']
+
         bad_channels = ['nodefaults']
         self.channels = [
             chan for chan in self.env_deps['environment']["channels"] if
             chan not in bad_channels ]
 
-        self.extended_data = None
-
-        #currently only support these platforms
-        self.platforms = ['linux-64', 'noarch']
         self.manifest = None
+        self.extended_data = None
         self.all_repo_data = None
+
         self.channel_root = pathlib.Path(channel_root)
+
+        #TODO: obsolete this
         self.channel_name = pathlib.PurePath(channel_name).name
         self.local_channel = self.channel_root / self.channel_name
 
-        self._requires_fetch = True
-
-    #TODO: make this idempotent
     def solve_environment(self):
         if not self.env_deps.get('solution', None):
             solution = solve_specs_for_arch(
-                "conda", 
-                self.env_deps['channels'], 
+                "conda",
+                self.env_deps['channels'],
                 specs=self.env_deps['specs'],
                 platform='linux-64'
             )
             self.env_deps['solution'] = solution
 
         return self.env_deps['solution']['actions']['FETCH']
-
-    def get_manifest(self):
-        if self.manifest:
-            return self.manifest
-
-        fetch_actions = self.solve_environment()
-        vendor_manifest_list = []
-        for conda_lock_fetch_info in fetch_actions:
-            url = conda_lock_fetch_info['url']
-            name = conda_lock_fetch_info['fn']
-            validation_value = conda_lock_fetch_info['sha256']
-            validation = {'type': 'sha256', 'value': validation_value}
-            vendor_manifest_list.append(
-                    {
-                        'url': url,
-                        'name': name,
-                        'validation': validation
-                    }
-            )
-        self.manifest = {'resources': vendor_manifest_list}
-        return self.manifest
 
 # typical FETCH entry
 # {
@@ -103,23 +87,21 @@ class CondaChannel:
 #         "version": "2021.7.5"
 #       },
 
-
-
     def get_extended_data(self):
-        if self.extended_dta:
-            return self.extended_data 
+        if self.extended_data:
+            return self.extended_data
 
         fetch_data =  self.solve_environment()
         extended_data = {}
-        
+
         for chan in self.channels:
             extended_data[chan] = {
-                'linux-64': 
+                'linux-64':
                 {
-                    'repodata_url': [], 
+                    'repodata_url': [],
                     'entries': []
-                }, 
-                'noarch': 
+                },
+                'noarch':
                 {
                     'repodata_url': [],
                     'entries': []
@@ -130,15 +112,35 @@ class CondaChannel:
             (channel, subdir) = entry['channel'].split('/')[-2:]
             extended_data[channel][subdir]['repodata_url'].append(entry['channel'] + '/repodata.json')
             extended_data[channel][subdir]['entries'].append(entry)
-       
+
         #remove dups
         for chan in self.channels:
             for subdir in ['linux-64', 'noarch']:
                 extended_data[chan][subdir]['repodata_url'] = list(set(extended_data[chan][subdir]['repodata_url']))
-                            
+
         self.extended_data = extended_data
         return self.extended_data
 
+    def get_manifest(self):
+        if self.manifest:
+            return self.manifest
+
+        fetch_actions = self.solve_environment()
+        vendor_manifest_list = []
+        for conda_lock_fetch_info in fetch_actions:
+            url = conda_lock_fetch_info['url']
+            name = conda_lock_fetch_info['fn']
+            validation_value = conda_lock_fetch_info['sha256']
+            validation = {'type': 'sha256', 'value': validation_value}
+            vendor_manifest_list.append(
+                    {
+                        'url': url,
+                        'name': name,
+                        'validation': validation
+                    }
+            )
+        self.manifest = {'resources': vendor_manifest_list}
+        return self.manifest
 
 
     def create_manifest(self, *, manifest_filename=None):
@@ -154,18 +156,19 @@ class CondaChannel:
             yaml.dump(manifest, f, sort_keys=False)
         return manifest
 
-
     def get_local_environment_yaml(self, *, local_environment_name=None):
-    
         local_yml = self.env_deps['environment'].copy()
         if not local_environment_name:
             local_environment_name = f"local_{local_yml['name']}"
 
         local_yml['name'] = local_environment_name
-        local_yml["channels"] = [
-                f"file://{self.local_channel.absolute()}",
-                "nodefaults"
-                ]
+        channel_paths = []
+        for chan in self.channels:
+            local_chan = self.channel_root / self.local_channel_name(chan)
+            channel_paths.append(f'file://{local_chan.absolute()}')
+        channel_paths.append('nodefaults')
+
+        local_yml["channels"] = channel_paths
         return local_yml
 
     def create_local_environment_yaml(self, *,
@@ -182,7 +185,6 @@ class CondaChannel:
         outpath_file_name = self.channel_root / cleaned_name
         with open(outpath_file_name, "w") as f:
             yaml.safe_dump(local_yml,f , sort_keys=False)
-        
         return local_yml
 
 # typical repodata entry
@@ -208,88 +210,81 @@ class CondaChannel:
 #   },
 
     def fetch_and_filter(self, subdir, extended_repo_data):
-         repodata = {
-             "info" : {"subdir" : subdir},
-             "packages": {},
-             "packages.conda": {}
-            }
+        repo_data = {
+                "info" : {"subdir" : subdir},
+                "packages": {},
+                "packages.conda": {}
+                }
 
         if not extended_repo_data['repodata_url']:
-            return repodata
+            return repo_data
 
-        valid_names = [ entry['fn'] for entry in extended_repo_data['entries'] ]
+        valid_names = [entry['fn'] for entry in extended_repo_data['entries']]
 
         repo_data_url = extended_repo_data['repodata_url'][0]
+        live_repo_data_json = improved_download(repo_data_url).json()
 
-        live_repo_data_json = improved_download(repodata_url).json()
-        for name, entry in live_repo_data_json['packages'].items():
-            if name in valid_names:
-                repodata['packages'][name] = entry
+        if live_repo_data_json.get('packages'):
+            for name, entry in live_repo_data_json['packages'].items():
+                if name in valid_names:
+                    repo_data['packages'][name] = entry
 
-        for name, entry in live_repo_data_json['packages.conda'].items():
-            if name in valid_names:
-                repodata['packages.conda'][name] = entry
-       
-        return repodata
+        if live_repo_data_json.get('packages.conda'):
+            for name, entry in live_repo_data_json['packages.conda'].items():
+                if name in valid_names:
+                    repo_data['packages.conda'][name] = entry
+
+        return repo_data
 
     def get_all_repo_data(self):
         if self.all_repo_data:
             return self.all_repo_data
 
-        extended_data = self.get_all_repo_data()
+        extended_data = self.get_extended_data()
         all_repo_data = {}
         for chan in self.channels:
             all_repo_data[chan] = {
-                "linux-64" : 
-                    self.fetch_and_filter('linux-64', extended_data[chan]['linux-64']), 
-                "noarch" : 
+                "linux-64" :
+                    self.fetch_and_filter('linux-64', extended_data[chan]['linux-64']),
+                "noarch" :
                     self.fetch_and_filter('noarch', extended_data[chan]['noarch'])
                 }
-        self.all_repo_data = all_repo_data                
+        self.all_repo_data = all_repo_data
         return self.all_repo_data
-   
-    def get_arch_repo_data(self, repo_type):
-        all_repo_data = self.get_all_repo_data()
-        return all_repo_data[repo_type]
 
-    def write_arch_repo_data(self, repo_type):
-        all_repo_data = self.get_all_repo_data()
+    #TODO: visit this rule?
+    def local_channel_name(self, chan: str):
+        return 'local_' + chan
 
-        dest_dir = self.local_channel / repo_type
-        dest_dir.mkdir(parents=True, exist_ok=True)
-        logger.info(f"creating repository {repo_type} at {dest_dir}")
+    def local_dir(self, chan, subdir):
+        chan_name = self.local_channel_name(chan)
+        return self.channel_root / chan_name / subdir
 
-        repo_data = all_repo_data[repo_type]
+    def make_local_dir(self, chan, subdir):
+        dest_dir = self.local_dir(chan, subdir)
+        dest_dir.mkdir(parents = True, exist_ok = True)
+        return dest_dir
+
+    def write_arch_repo_data(self, chan, subdir, repo_data):
+        dest_dir = self.make_local_dir(chan, subdir)
         dest_file = dest_dir / 'repodata.json'
-        with open(dest_file, 'w') as f:
-            logger.info(f"Writing Repodata to {dest_file}")
-            json.dump(repo_data, f, indent=1)
+        with dest_file.open('w') as f:
+            json.dump(repo_data, f)
 
     def write_repo_data(self):
         all_repo_data = self.get_all_repo_data()
-        for repo_type in all_repo_data.keys():
-            self.write_arch_repo_data(repo_type)
-
+        for chan, platform in all_repo_data.items():
+            for subdir, repo_data in platform.items():
+                self.write_arch_repo_data(chan, subdir, repo_data)
 
     @staticmethod
     def _calc_sha256(byte_array):
         return hashlib.sha256(byte_array).hexdigest()
 
     @staticmethod
-    def improved_download(url):
-        session = requests.Session()
-        retry = Retry(connect=5, backoff_factor=0.5)
-        adapter = HTTPAdapter(max_retries=retry)
-        session.mount('http://', adapter)
-        session.mount('https://', adapter)
-        return session.get(url)
-
-
-    @staticmethod
     def download_and_validate(out: pathlib.Path, url, sha256):
-        
         logger.info(f'downloading {url} to {out}')
-        response = CondaChannel.improved_download(url)
+        response = improved_download(url)
         url_data = response.content
         with open(out, "wb") as f:
             f.write(url_data)
@@ -300,31 +295,18 @@ class CondaChannel:
                     f"SHA256 does not match for {out} calculated SHA: {calculated_sha}, "
                     "manifest SHA: {sha256}"
                 )
-        
-    @staticmethod
-    def _get_arch_type(url):
-        if '/noarch/' in url:
-            return 'noarch'
-        elif '/linux-64/' in url:
-            return 'linux-64'
-        else:
-            raise RuntimeError(f"don't know where to put {url}")
+
+    def download_arch_binaries(self, chan, subdir, entries):
+        dest_dir = self.make_local_dir(chan, subdir)
+        for entry in entries:
+            self.download_and_validate(dest_dir / entry['fn'],
+                entry['url'], entry['sha256'])
 
     def download_binaries(self):
-        paths = [self.local_channel / 'linux-64', self.local_channel / 'noarch' ]
-        for p in paths:
-            p.mkdir(parents=True, exist_ok=True)
-
-        manifest  = self.get_manifest()
-        for entry in manifest['resources']:
-            url = entry['url']
-                    
-            subdir = self._get_arch_type(url)
-            dest_dir = self.local_channel / subdir
-            dest_file = dest_dir / entry['name']
-
-            self.download_and_validate(dest_file, url, entry['validation']['value'])
- 
+        extended_data = self.get_extended_data()
+        for chan, platform in extended_data.items():
+            for subdir, rest in platform.items():
+                self.download_arch_binaries(chan, subdir, rest['entries'])
 
 def get_manifest(conda_channel: CondaChannel):
     return conda_channel.get_manifest()
@@ -343,7 +325,7 @@ def get_local_environment_yaml(
 ):
     return conda_channel.get_local_environment_yaml(
         local_environment_name=local_environment_name)
-    
+
 def create_local_environment_yaml(
     conda_channel: CondaChannel,
     *,
@@ -362,7 +344,7 @@ def create_local_channels(
     manifest_filename = None,
     local_environment_name = None,
     local_environment_filename = None,
-):   
+):
     conda_channel.create_manifest(
         manifest_filename=manifest_filename
     )
@@ -370,6 +352,6 @@ def create_local_channels(
         local_environment_name=local_environment_name,
         local_environment_filename=local_environment_filename
         )
-    conda_channel.write_repo_data()        
+    conda_channel.write_repo_data()
     conda_channel.download_binaries()
-    
+
