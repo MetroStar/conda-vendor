dcf45e89763cab9a1715b21aa00bfbea6391bbe2
George Hoersting <ghoersting@metrostarsystems.com>
2021-07-14 13:02:35 -0400

    Updated Readme

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 from setuptools import find_packages , setup 
+#TODO: we can make this a conda package and have but that will be on the next iteration.
+#TODO: Some more work can be done on the setup requires but for now use the conda_vendor_test.yml
+#if you want to build the wheel. 
 
-#https://stackoverflow.com/questions/57821903/setup-py-with-dependecies-installed-by-conda-not-pip
-#we need a meta.yaml and to remove conda lock from here, if we want the conda package
-#yaml is a conda package 
-##durrr
 setup (name='conda_vendor',
        version='0.1',
        package_dir={"": "."},
