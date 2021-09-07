6fbd46cf43ef8a4ee15e42adf537a441befe2e4e
t-potts <tylerjacobpotts@gmail.com>
2021-09-07 12:18:07 -0500

    Updated CLI

--- conda_vendor/__main__.py
+++ conda_vendor/__main__.py
@@ -6,6 +6,7 @@ from conda_vendor.cli import (
 )
 
 import click
+from pathlib import Path
 
 
 @click.group()
@@ -13,7 +14,7 @@ def cli():
     pass
 
 
-@click.command(help="creates a meta-manifest file from an environment yaml file")
+@click.command(help="meta-manifest file from an environment yaml file")
 @click.option(
     "--environment-yaml",
     default="environment.yaml",
@@ -27,7 +28,7 @@ def cli():
     default="meta_manifest.yaml",
     help="filename of output manifest",
 )
-def create_manifests(environment_yaml, manifest_root, manifest_filename):
+def create_meta_manifest(environment_yaml, manifest_root, manifest_filename):
 
     click.echo(manifest_root)
     click.echo(manifest_filename)
@@ -39,7 +40,7 @@ def create_manifests(environment_yaml, manifest_root, manifest_filename):
     )
 
 
-@click.command(help="create local channels from an input meta-manifest file")
+@click.command(help="local channels from meta-manifest file")
 @click.option(
     "--channel-root",
     default="./local_channel",
@@ -56,8 +57,63 @@ def create_channels(channel_root, meta_manifest_path):
     create_local_channels_from_meta_manifest(channel_root, meta_manifest_path)
 
 
-cli.add_command(create_manifests)
+@click.command(help="custom manifest from meta-manifest file")
+@click.option(
+    "--manifest-type", default="iron-bank", help="type of custom manifest to create"
+)
+@click.option(
+    "--meta-manifest-path",
+    default="./meta_manifest.yaml",
+    help="path to meta manifest file",
+)
+@click.option(
+    "--output-manifest-dir",
+    default="./",
+    help="output manifest dir",
+)
+def create_custom_manifest(manifest_type, meta_manifest_path, output_manifest_dir):
+    meta_manifest_path = Path(meta_manifest_path)
+    output_manifest_dir = Path(output_manifest_dir)
+
+    if manifest_type == "iron-bank":
+        create_ironbank_from_meta_manifest(
+            meta_manifest_path=meta_manifest_path,
+            output_manifest_dir=output_manifest_dir,
+        )
+    else:
+        error_str = f'Manifest type "{manifest_type}" not supported'
+        raise TypeError(error_str)
+
+
+@click.command(
+    help="""conda environment.yaml file based on the metamanifest, 
+    and path to the local channels"""
+)
+@click.option(
+    "--channel-root",
+    default="./local_channel",
+    help="directory where local channels are stored, and directory where the environment yaml will be output",
+)
+@click.option(
+    "--meta-manifest-path",
+    default="./meta_manifest.yaml",
+    help="path to meta manifest file",
+)
+@click.option(
+    "--env-name",
+    default="conda-vendor-env",
+    help="name of conda environment defined in yaml",
+)
+def create_local_yaml(channel_root, meta_manifest_path, env_name):
+    channel_root = Path(channel_root)
+    meta_manifest_path = Path(meta_manifest_path)
+    create_yaml_from_manifest(channel_root, meta_manifest_path, env_name)
+
+
+cli.add_command(create_meta_manifest)
 cli.add_command(create_channels)
+cli.add_command(create_custom_manifest)
+cli.add_command(create_local_yaml)
 
 
 if __name__ == "__main__":
