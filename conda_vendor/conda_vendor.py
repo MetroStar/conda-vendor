772df57d77f357ee84a19987df96ba1d760201d2
rigzba21 <jonathan.velando01@gmail.com>
2022-04-09 17:14:24 -0400

    adding function to create vendored channel directory structure
    Signed-off-by: rigzba21 <jonathan.velando01@gmail.com>

--- conda_vendor/conda_vendor.py
+++ conda_vendor/conda_vendor.py
@@ -2,6 +2,7 @@ import click
 import yaml
 import sys
 import struct
+import os
 from conda_vendor.version import __version__
 from conda_vendor.conda_lock_wrapper import CondaLockWrapper
 from conda_lock.src_parser import LockSpecification
@@ -10,9 +11,47 @@ from pathlib import Path
 from typing import List
 
 def get_lock_spec_for_environment_file(environment_file) -> LockSpecification:
-
     lock_spec = CondaLockWrapper.parse_environment_file(environment_file)
     return lock_spec
+
+
+# create the vendored channel directory, given the name in the environment.yaml
+def create_vendored_dir(environment_file, platform, desired_path=None):
+    with open(environment_file, 'r') as env_file:
+        try:
+            environment_yaml = yaml.safe_load(env_file)
+            environment_name = environment_yaml['name']
+        except yaml.YAMLError as err:
+            click.echo(err)
+            sys.exit(f"Failed to read environment name from {environment_file}")
+    
+    # use current working directory if no path specified
+    if desired_path == None:
+        try:
+            path = os.path.join(os.getcwd(), environment_name)
+            os.mkdir(path)
+            create_platform_dir(path, platform)
+            create_noarch_dir(path)
+        except FileExistsError as err:
+            click.echo(err)
+            sys.exit(f"Directory \"{environment_name}\" already exists")
+
+def create_platform_dir(path, platform):
+    try:
+        platform_path = os.path.join(path, platform)
+        os.mkdir(platform_path)
+    except FileExistsError as err:
+        click.echo(err)
+        sys.exit(f"Directory \"{platform_path}\" already exists")
+
+def create_noarch_dir(path):
+    try:
+        noarch_path = os.path.join(path, 'noarch')
+        os.mkdir(noarch_path)
+    except FileExistsError as err:
+        click.echo(err)
+        sys.exit(f"Directory \"{noarch_path}\" already exists")
+   
 
 def solve_environment(lock_spec, solver, platform) -> DryRunInstall:
     specs = get_specs(lock_spec)
@@ -98,12 +137,14 @@ def vendor(file,solver, platform):
     
     # handle environment.yaml
     environment_yaml = Path(file)
+    create_vendored_dir(environment_yaml, platform)
+
     lock_spec = get_lock_spec_for_environment_file(environment_yaml)
     
+
     dry_run_install = solve_environment(lock_spec, solver, platform)
     
     fetch_action_packages = get_fetch_actions(dry_run_install)
-
 
 
 main.add_command(vendor)
