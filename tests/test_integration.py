293c28be17508f2617dbc6784ced86214a453b6e
rigzba21 <jonathan.velando01@gmail.com>
2022-04-11 09:07:33 -0400

    adding test fixture, and checking json output in integration test
    Signed-off-by: rigzba21 <jonathan.velando01@gmail.com>

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import json
 from unittest import TestCase
 from unittest.mock import Mock, patch
 from ruamel.yaml import YAML
@@ -15,3 +16,8 @@ def test_vendor_dry_run(python_main_defaults_environment):
     assert result.exit_code == 0
     assert 'Dry Run - Will Not Download Files' in result.output
     assert 'Dry Run Complete!' in result.output
+
+    json_output = result.output.split('Dry Run Complete!', 1)[1]
+    fetch_actions = json.loads(json_output)
+    assert any(pkg['name'] == 'python' for pkg in fetch_actions)
+    assert not any(pkg['name'] == 'tensorflow' for pkg in fetch_actions)
