e0f22bf68d85f54be3af431b932f2033e061e515
Johnny Bouder <jbouder@metrostar.com>
2024-09-19 09:33:15 -0400

    Fix github action. Remove test script.

--- run.sh
+++
@@ -1,8 +0,0 @@
-conda deactivate conda-vendor-dependencies
-conda env remove -n conda-vendor-dependencies
-
-conda env create --file environment.yaml
-conda activate conda-vendor-dependencies
-pip install -e .
-
-pytest tests/ -vvv -s
